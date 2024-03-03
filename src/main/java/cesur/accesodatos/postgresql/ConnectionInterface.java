157c3382db01e3a9b6c270e2ab400a63aba84c96
Isaac Lolade <isaaclolade12@gmail.com>
2024-03-03 21:45:28 +0100

    PostgreSQL Componentes v.1.5

--- src/main/java/cesur/accesodatos/postgresql/ConnectionInterface.java
+++ src/main/java/cesur/accesodatos/postgresql/ConnectionInterface.java
@@ -1,6 +1,22 @@
 package cesur.accesodatos.postgresql;
 
+/**
+ * Interface to manage any kind of database connections.
+ *
+ * This interface is meant to be implemented in any piece of software that requires a database connection.
+ * Its implementation will depend on the DBMS.
+ *
+ * @author Carlos Sánchez Recio.
+ */
 public interface ConnectionInterface {
+    /**
+     * Method to execute all connection process.
+     * @return Boolean value to indicate of the connection was successful or not
+     */
     public boolean connectDB();
+
+    /**
+     * Method to close connection with the database server
+     */
     public void closeConnection();
 }
